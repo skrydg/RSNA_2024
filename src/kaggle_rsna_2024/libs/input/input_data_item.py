03c3b7a082620341fb97fbd0d92fe0aae9aeee16
skrydg <skrrydg@yandex.ru>
2024-06-10 21:48:03 +0400

    fx

--- src/kaggle_rsna_2024/libs/input/input_data_item.py
+++ src/kaggle_rsna_2024/libs/input/input_data_item.py
@@ -3,6 +3,7 @@ import os
 
 from kaggle_rsna_2024.libs.env import Env
 from kaggle_rsna_2024.libs.input.input_3d_scan import Input3dScan
+from collections import defaultdict
 
 class InputDataItem:
     def __init__(self, directory: Path, study_id: int, series_info: dict, image_shape: tuple):
@@ -10,7 +11,7 @@ class InputDataItem:
         self.study_id = study_id
         self.image_shape = image_shape
 
-        self.scans = {}
+        self.scans = defaultdict(list)
         for series_id, scan_type in series_info.items():
             series_directory = self.directory / str(series_id)
-            self.scans[scan_type] = Input3dScan(series_directory, study_id, series_id, scan_type, image_shape)
+            self.scans[scan_type].append(Input3dScan(series_directory, study_id, series_id, scan_type, image_shape))
