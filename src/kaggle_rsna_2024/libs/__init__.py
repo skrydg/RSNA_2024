8e56cbdf4bc34659f0815e954e656f3f23c43d88
skrydg <skrrydg@yandex.ru>
2024-06-15 12:37:58 +0400

    fix

--- src/kaggle_rsna_2024/libs/__init__.py
+++ src/kaggle_rsna_2024/libs/__init__.py
@@ -1,6 +1,6 @@
 from .input import *
 from .cache_caller import *
-from .scan_animation import ScanAnimation
+from .animation import *
 from .dataset import *
 from .kfold import TFDatasetKFold
 from .mean_log_loss import MeanLogLoss
